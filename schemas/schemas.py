68e474f33fb7fa21c3369a96e991de1c593e729a
Estevam1to <luisestevam33@gmail.com>
2024-10-25 16:57:31 -0300

    feat: criando schemas de requisição

---
+++ schemas/schemas.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+class Request(BaseModel):
+    SepalLengthCm: float
+    SepalWidthCm: float
+    PetalLengthCm: float
+    PetalWidthCm: float
+
+class Response(BaseModel):
+    category: str
