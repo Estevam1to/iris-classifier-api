289cb2a4846fd9b993d6812c975b8abc2b68dc7a
Estevam1to <luisestevam33@gmail.com>
2024-10-26 18:41:42 -0300

    feat: refatorar rota de predição e adicionar classe de predição

--- routes/predict.py
+++ routes/predict.py
@@ -1,20 +1,12 @@
-import pickle
-
 import numpy as np
 from fastapi import APIRouter
 
 from schemas.schemas import Response, Request
+from model.prediction import Prediction
 
 router = APIRouter(prefix="/api/predict", tags=["Predict"])
 
-
-async def predict(values: np.array) -> Response:
-    with open("path/modelo.pkl", "rb") as file:
-        model = pickle.load(file)
-
-    pred = model.predict(values.reshape(1, -1))[0]
-
-    return Response(category=pred)
+prediction = Prediction()
 
 
 @router.post(path="/", status_code=200, response_model=Response)
@@ -24,9 +16,10 @@ async def predict_endpoint(request: Request) -> Response:
             request.SepalLengthCm,
             request.SepalWidthCm,
             request.PetalLengthCm,
-            request.PetalWidthCm
-         ]
+            request.PetalWidthCm,
+        ]
     )
 
-    response = await predict(values)
-    return response
+    category = prediction.predict(values)
+
+    return Response(category=category)
